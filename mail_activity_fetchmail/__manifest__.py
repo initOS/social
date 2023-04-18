afd7da8dbebb414b2326bcee890f895dac68dac1
Ruben Ortlam <ruben.ortlam@initos.com>
2023-04-18 14:43:11 +0200

    [ADD] mail_activity_fetchmail module

---
+++ mail_activity_fetchmail/__manifest__.py
@@ -0,0 +1,17 @@
+# © 2022 initOS GmbH
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Mail Activity on Fetchmail",
+    "version": "14.0.1.0.0",
+    "category": "Hidden",
+    "author": "initOS GmbH",
+    "website": "https://www.initos.com",
+    "license": "AGPL-3",
+    "summary": "Uses mail.activity.team to configure automatic activities when mails "
+    "arrive for the specified models",
+    "depends": [
+        "mail_activity_team",
+    ],
+    "installable": True,
+}
